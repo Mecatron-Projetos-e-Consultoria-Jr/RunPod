ca48e0f592fbbc12d25fb6f63ce605b165481000
Pedro4064 <pedrohlcruz@gmail.com>
2020-10-02 10:40:47 -0300

    Both Slav Smoothing and average smoothing

--- RunPod.ino
+++ RunPod.ino
@@ -1,18 +1,26 @@
 // Include the necessary libraries 
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <SavLayFilter.h>
 #include <Wire.h>
 
 // Instantiate the accelerometer class
 Adafruit_MPU6050 mpu;
 
+
+double z_gyro;
+double last_check = 0;
+double derivative = 0;
+
+
+//SavLayFilter smallFilter (&value, 0, 5);             //Cubic smoothing with windowsize of 5
+SavLayFilter largeFilter (&z_gyro, 0, 25);            //Cubic smoothing with windowsize of 25
+SavLayFilter derivativeFilter (&derivative, 0, 25); 
+
+
 void setup(void) {
 
     Serial.begin(115200);
-    while (!Serial) {
-        delay(10); // will pause Zero, Leonardo, etc until serial console opens
-    }
-
 
     // Try to initialize, if it fails it will continue to try and throw an error message
     if (!mpu.begin()) {
@@ -31,35 +39,53 @@ void setup(void) {
 
 void loop() {
 
-  float smoothed_data[] = {0,0,0,0,0,0,0,0,0,0};
-  for (auto& smoothed:smoothed_data){
-    float x_values[] = {0,0,0,0,0,0,0,0,0,0};
-    
-    for (auto& value:x_values){
-      /* Get new sensor events with the readings */
-      sensors_event_t accel, gyro, temp;
-      mpu.getEvent(&accel, &gyro, &temp);
-      value = accel.acceleration.x;
-  
-    }
-  
-    float average = 0;
-    
-    for (auto& reading:x_values){
-      average+=reading;
-    }
-  
-    smoothed = average/10;
-    
-    
+    double z_array[5];
+    double d_array[10];
 
-  } 
+    for(auto& z:z_array){
+        /* Get new sensor events with the readings */
+        sensors_event_t accel, gyro, temp;
+        mpu.getEvent(&accel, &gyro, &temp);
+        z_gyro = gyro.gyro.z;
 
-  float average = 0;
-   for (auto& smoothed:smoothed_data){
-      average+=smoothed;
+        
+        // Print the raw data
+        // Serial.print("Raw_Data:");
+        // Serial.print(z_gyro);
+        
+        // Serial.print(",");
+        
+        // Compute the Savitzky-Golay Filtering Algorithm to smooth the z angular acceleration
+        z_gyro = largeFilter.Compute();
+        z_gyro = largeFilter.Compute();
+        // Serial.print("Smoothed_Data_1:");
+        // Serial.print(z_gyro);
+
+        // Add the smoothed data to the array
+        z = z_gyro;
+    
+        
+        
     }
 
-  Serial.print("Smoothed_data:");
-  Serial.println(average/10);
+    // Sum the z data and make an average
+    double sum = 0;
+    for(auto& n:z_array){
+        sum+=n;
+    }
+
+    double average = sum/5;
+    Serial.print("Average_5:");
+    Serial.print(average);
+    Serial.print(",");
+
+    derivative = average - last_check;
+    derivative = derivativeFilter.Compute();
+    derivative = derivativeFilter.Compute();
+    derivative = derivativeFilter.Compute();
+    Serial.print("Derivative:");
+    Serial.println(derivative * 10);
+    
+    last_check = average;
+    
 }
