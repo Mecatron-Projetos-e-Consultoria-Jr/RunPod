3320f41a06b53931be6dda8c5c459ac4ff56cf1d
Pedro4064 <pedrohlcruz@gmail.com>
2021-02-09 09:21:06 -0300

    [DELETED] Old filed used with the esp-32

--- DataAnalysis.h
+++
@@ -1,5 +0,0 @@
-namespace Data_Analysis
-{
-    // Function that returns true when the x value is inside the thrashold and false otherwise
-    bool inside_thrashold(double x_average, const double thrashold); 
-} // namespace Data_Analysis
